a3bb1b035e8eb800e383915f542d82d2a59886f1
Thierry <varelathierry@gmail.com>
2022-07-13 21:06:00 -0300

    criando validacao para o botao enviar

--- script.js
+++ script.js
@@ -9,17 +9,25 @@ function click(event) {
   }
 }
 
-function countCharecter() {
-  const textarea = document.getElementById('text-area');
+function countCharacter() {
+  const textarea = document.getElementById('textarea');
   const count = document.getElementById('counter');
   const text = textarea.value;
   count.innerText = (500 - text.length);
 }
+function enviar(event) {
+  const agree = document.getElementById('agreement');
+  if (agree.checked === false) {
+    event.preventDefault();
+  }
+}
 
 function initialize() {
   const enviarForm = document.querySelector('#login');
-  const textarea = document.getElementById('text-area');
+  const textarea = document.getElementById('textarea');
+  const buttonEnviar = document.getElementById('submit-btn');
   enviarForm.addEventListener('click', click);
-  textarea.addEventListener('keyup', countCharecter);
+  textarea.addEventListener('keyup', countCharacter);
+  buttonEnviar.addEventListener('click', enviar);
 }
 initialize();
