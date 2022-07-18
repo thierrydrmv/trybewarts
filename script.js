fd487287b4e028d7e1b643960e105e0e9857f231
Thierry <varelathierry@gmail.com>
2022-07-18 20:59:03 -0300

    exercicio 21 incompleto

--- script.js
+++ script.js
@@ -25,14 +25,32 @@ function enviar() {
   }
 }
 
+function selectContent(lista) {
+  let texto = '';
+  for (let index = 0; index < lista.length; index += 1) {
+    texto = `${texto} ${lista[index].value}, `;
+  }
+  return texto;
+}
+
+function recolheDados() {
+  const house = document.getElementById('house').value;
+  const family = document.querySelector('input[name="family"]:checked').value;
+  const content = document.querySelectorAll('input[class="subject"]:checked');
+  const contentValue = selectContent(content);
+  console.log(contentValue);
+}
+
 function initialize() {
-  const enviarForm = document.querySelector('#login');
+  const enviarForm = document.querySelector('#login');  
   const textarea = document.getElementById('textarea');
   const agree = document.getElementById('agreement');
   const buttonEnviar = document.getElementById('submit-btn');
+  buttonEnviar.addEventListener('click', recolheDados);
   buttonEnviar.disabled = true;
   enviarForm.addEventListener('click', click);
   textarea.addEventListener('keyup', countCharacter);
   agree.addEventListener('click', enviar);
 }
+
 initialize();
